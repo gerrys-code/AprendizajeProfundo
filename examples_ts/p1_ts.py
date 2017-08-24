4267e9b9e02a28e1a626a4d5c7520b80c167b809
gerrys-code <gerry_math@comunidad.unam.mx>
2017-08-24 10:46:50 -0500

    Create a session for run a node from the Graph

--- examples_ts/p1_ts.py
+++ examples_ts/p1_ts.py
@@ -5,3 +5,15 @@ import tensorflow as tf
 a = tf.add(2,3)
 
 print (a)
+
+
+#sess = tf.Session()
+#print (sess.run(a))
+#sess.close()
+
+""" A Session object encapsulates the enviroment in which
+Operations objects are exacuted, and Tensor objects are
+evaluated.
+"""
+with tf.Session() as sess:
+    print (sess.run(a))
